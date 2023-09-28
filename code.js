af471087e6b2a1d0c00d0577f5ad6e4d3b319691
edvardsanta <eduardosantamarco123@outlook.com>
2023-09-27 23:29:51 -0300

    formatting and stoping audio

--- code.js
+++ code.js
@@ -1,64 +1,61 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-const state = 
-{
+const state = {
   circle: ctx,
   rectGreen: ctx,
   rectWhite: ctx,
   text: ctx,
-  rhombus: ctx  
-}
+  rhombus: ctx,
+};
 
-function createRectGreen()
-{
+function createRectGreen() {
   state.rectGreen.fillStyle = "#009c3b";
   state.rectGreen.fillRect(0, 0, 2000, 2000);
 }
 
-function createCircle()
-{
+function createCircle() {
   state.circle.beginPath();
-  state.circle.arc(500, 250, 150, 0, 2 * Math.PI);
+  state.circle.arc(500, 250, 150, 0, 2 * Math.PI);
   state.circle.fillStyle = "#002776";
   state.circle.fill();
 }
 
-function createRectWhite()
-{
-  state.rectWhite.fillStyle = "#FFFFFF";	
+function createRectWhite() {
+  state.rectWhite.fillStyle = "#FFFFFF";
   state.rectWhite.fillRect(350, 220, 300, 50);
 }
 
-function Write()
-{
-  state.text.font = "35px Arial";
-  state.text.strokeStyle = "#009c3b";
-  state.text.textAlign = "center";
-  state.text.strokeText("Ordem e progresso", 500, 255);
+function Write() {
+  state.text.font = "35px Arial";
+  state.text.strokeStyle = "#009c3b";
+  state.text.textAlign = "center";
+  state.text.strokeText("Ordem e progresso", 500, 255);
 }
 
-function createRhombus()
-{
-  let rhombus1 =  state.rhombus; 
+function createRhombus() {
+  let rhombus1 = state.rhombus;
   rhombus1.strokeStyle = "#ffdf00";
   rhombus1.moveTo(450, 105);
   rhombus1.lineTo(200, 250);
   rhombus1.lineTo(450, 400);
   rhombus1.stroke();
-   
-  let rhombus2 =  state.rhombus; 
+
+  let rhombus2 = state.rhombus;
   rhombus2.strokeStyle = "#ffdf00";
   rhombus2.moveTo(550, 400);
   rhombus2.lineTo(800, 250);
   rhombus2.lineTo(555, 105);
   rhombus2.stroke();
- 
 }
 
-function Audio()
-{
+function Audio() {
   const audio = document.getElementById("audio");
-  audio.play();
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+  }
 }
 
 createRectGreen();
